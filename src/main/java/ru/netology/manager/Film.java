1e75d94fa0de1a4bb57b02f3271d53328747850d
kseniyaaltuhova <bogomolowakseniya@yandex.ru>
2022-03-21 23:29:45 +0300

    add manager and plugins

---
+++ src/main/java/ru/netology/manager/Film.java
@@ -0,0 +1,11 @@
+package ru.netology.manager;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+
+public class Film {
+    private int id;
+    private String filmName;
+    private String filmGenre;
+}
