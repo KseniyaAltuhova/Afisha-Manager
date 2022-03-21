db8d9e5d59869d6dd2ef6d74029e3bd82e9acca8
kseniyaaltuhova <bogomolowakseniya@yandex.ru>
2022-03-21 23:52:50 +0300

    add afisha.repository and mockito plugin

--- src/main/java/ru/netology/manager/AfishaManager.java
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -1,28 +1,25 @@
 package ru.netology.manager;
 
-public class AfishaManager {
-    private Film[] items = new Film[0];
-    private int itemsToShow;
+import ru.netology.repository.AfishaRepository;
 
-    public AfishaManager(int itemsToShow) {
-        this.itemsToShow = itemsToShow;
+public class AfishaManager {
+    private AfishaRepository repository;
+
+    public AfishaManager(AfishaRepository repository) {
+        this.repository = repository;
     }
 
     public void add(Film item) {
-        int length = items.length + 1;
-        Film[] tmp = new Film[length];
-
-        for (int i = 0; i < items.length; i++) {
-            tmp[i] = items[i];
-        }
-
-        int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = item;
-        items = tmp;
+        repository.save(item);
     }
 
-    public Film[] getLastAddedItems() {
+    public Film[] getLastAddedItems(int itemsToShow) {
         int length = itemsToShow;
+        Film[] items = repository.findAll();
+
+        if (itemsToShow < 0) {
+            length = 0;
+        }
         if (length > items.length) {
             length = items.length;
         }
